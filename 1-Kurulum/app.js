b66c7451e78f93d834c4948fa9a1a73cf9529e9a
ALPER BİLGİN <alper.bilgin@hotmail.com>
2024-10-09 00:30:29 +0300

    Kurulum dosyaları eklendi

---
+++ 1-Kurulum/app.js
@@ -0,0 +1,4 @@
+console.log("Typescript e hoşgeldiniz");
+// Typescripte yapılan her değişiklik için " tsc app.ts " komutu terminalde çalıştırılır .
+// Her seferinde tekrar tekrar "tsc app.ts" çalıştırmamak için "tsc app.ts -w" komutu yazılabilir .
+console.log("ALPER BİLGİN");
